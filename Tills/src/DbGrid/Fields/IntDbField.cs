d2b911d015bdabc9202405f8bbfea0f0ee4ed905
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2024-03-24 13:23:46 +0000

    Added Age and Wage fields to employee db grid

---
+++ Tills/src/DbGrid/Fields/IntDbField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ComputingEPOS.Tills;
+
+public class IntDbField<T> : TextBoxDbField<T, int> {
+    public IntDbField(string label, string fieldName) : base(label, fieldName) { }
+    protected override int FromString(string? data) => int.Parse(data!);
+}
